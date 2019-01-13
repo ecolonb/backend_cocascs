3077ec262c9320a954df8e9e713b1011629ae5c3
Edgar Colón <edgar.colonb@gmail.com>
2019-01-12 18:42:49 -0600

    Se habilitan los CORS

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const mongoose = require('mongoose');
 require('./server/config/config');
 const routesApi = require('./routes/api');
 const routesWeb = require('./routes/web');
+//Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 // like to BodyParser
 app.use(express.urlencoded({ extended: true }));
 

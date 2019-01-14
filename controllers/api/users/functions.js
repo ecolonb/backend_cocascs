ae3fbd35c9c1b9629b779094fa9bc7e247498e41
Edgar Colón <edgar.colonb@gmail.com>
2019-01-14 12:01:49 -0600

    Se modifica el formato del email_4

--- controllers/api/users/functions.js
+++ controllers/api/users/functions.js
@@ -31,69 +31,100 @@ const fnSendEmail = async (hashToValidateAcount, typePlayer, name, email) => {
   console.log('urlEndPoint', urlEndPoint);
   if (typePlayer == 'player') {
     message = `
-    <style>
-    .myButton {
-      -moz-box-shadow: 0px 1px 0px 0px #f0f7fa;
-      -webkit-box-shadow: 0px 1px 0px 0px #f0f7fa;
-      box-shadow: 0px 1px 0px 0px #f0f7fa;
-      background: -webkit-gradient(
-        linear,
-        left top,
-        left bottom,
-        color-stop(0.05, #33bdef),
-        color-stop(1, #019ad2)
-      );
-      background: -moz-linear-gradient(top, #33bdef 5%, #019ad2 100%);
-      background: -webkit-linear-gradient(top, #33bdef 5%, #019ad2 100%);
-      background: -o-linear-gradient(top, #33bdef 5%, #019ad2 100%);
-      background: -ms-linear-gradient(top, #33bdef 5%, #019ad2 100%);
-      background: linear-gradient(to bottom, #33bdef 5%, #019ad2 100%);
-      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#33bdef', endColorstr='#019ad2',GradientType=0);
-      background-color: #33bdef;
-      -moz-border-radius: 6px;
-      -webkit-border-radius: 6px;
-      border-radius: 6px;
-      border: 1px solid #057fd0;
-      display: inline-block;
-      cursor: pointer;
-      color: #ffffff;
-      font-family: Arial;
-      font-size: 15px;
-      font-weight: bold;
-      padding: 6px 55px;
-      text-decoration: none;
-      text-shadow: 0px -1px 0px #5b6178;
-    }
-    .myButton:hover {
-      background: -webkit-gradient(
-        linear,
-        left top,
-        left bottom,
-        color-stop(0.05, #019ad2),
-        color-stop(1, #33bdef)
-      );
-      background: -moz-linear-gradient(top, #019ad2 5%, #33bdef 100%);
-      background: -webkit-linear-gradient(top, #019ad2 5%, #33bdef 100%);
-      background: -o-linear-gradient(top, #019ad2 5%, #33bdef 100%);
-      background: -ms-linear-gradient(top, #019ad2 5%, #33bdef 100%);
-      background: linear-gradient(to bottom, #019ad2 5%, #33bdef 100%);
-      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#019ad2', endColorstr='#33bdef',GradientType=0);
-      background-color: #019ad2;
-    }
-    .myButton:active {
-      position: relative;
-      top: 1px;
-    }
-  </style>
-  <div style="width: 100%;text-align: center;font-size: 17px;">
-    <p>
-      Hola ${name} estás registrado como participante en Cocas Copiloto Satelital, recuerda meter muchas
-      cocas y compartir muchos momentos felices!
-    </p>
-    <p style="display:inline-block;width:100%; text-align:center;">
-      <a href="${urlEndPoint}" class="myButton">Activar cuenta</a>
-    </p>
-  </div>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+    <html xmlns="http://www.w3.org/1999/xhtml">
+      <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Demystifying Email Design</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <style>
+          .myButton {
+            -moz-box-shadow: 0px 1px 0px 0px #f0f7fa;
+            -webkit-box-shadow: 0px 1px 0px 0px #f0f7fa;
+            box-shadow: 0px 1px 0px 0px #f0f7fa;
+            background: -webkit-gradient(
+              linear,
+              left top,
+              left bottom,
+              color-stop(0.05, #33bdef),
+              color-stop(1, #019ad2)
+            );
+            background: -moz-linear-gradient(top, #33bdef 5%, #019ad2 100%);
+            background: -webkit-linear-gradient(top, #33bdef 5%, #019ad2 100%);
+            background: -o-linear-gradient(top, #33bdef 5%, #019ad2 100%);
+            background: -ms-linear-gradient(top, #33bdef 5%, #019ad2 100%);
+            background: linear-gradient(to bottom, #33bdef 5%, #019ad2 100%);
+            filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#33bdef', endColorstr='#019ad2',GradientType=0);
+            background-color: #33bdef;
+            -moz-border-radius: 6px;
+            -webkit-border-radius: 6px;
+            border-radius: 6px;
+            border: 1px solid #057fd0;
+            display: inline-block;
+            cursor: pointer;
+            color: #ffffff;
+            font-family: Arial;
+            font-size: 15px;
+            font-weight: bold;
+            padding: 6px 55px;
+            text-decoration: none;
+            text-shadow: 0px -1px 0px #5b6178;
+          }
+          .myButton:hover {
+            background: -webkit-gradient(
+              linear,
+              left top,
+              left bottom,
+              color-stop(0.05, #019ad2),
+              color-stop(1, #33bdef)
+            );
+            background: -moz-linear-gradient(top, #019ad2 5%, #33bdef 100%);
+            background: -webkit-linear-gradient(top, #019ad2 5%, #33bdef 100%);
+            background: -o-linear-gradient(top, #019ad2 5%, #33bdef 100%);
+            background: -ms-linear-gradient(top, #019ad2 5%, #33bdef 100%);
+            background: linear-gradient(to bottom, #019ad2 5%, #33bdef 100%);
+            filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#019ad2', endColorstr='#33bdef',GradientType=0);
+            background-color: #019ad2;
+          }
+          .myButton:active {
+            position: relative;
+            top: 1px;
+          }
+        </style>
+      </head>
+      <body>
+        <body style="margin: 0; padding: 0;">
+          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+              <td>
+                <label
+                  style="display: block;width: 100%;  text-align: center;margin-bottom: 10px;margin-top: 20px;font-size: 18px;"
+                  >Bienvenido a Cocas Copiloto Satelital</label
+                >
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <span
+                  style="display: block;width: 100%;  text-align: center;margin-bottom: 14px;font-size: 18px;"
+                >
+                  Hola ${name} estás registrado como participante, recuerda meter
+                  muchas cocas y comparpirt muchos momentos felices!!
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p style="display:inline-block;width:100%; text-align:center;">
+                  <a href="${urlEndPoint}" class="myButton">Activar cuenta</a>
+                </p>
+              </td>
+            </tr>
+          </table>
+        </body>
+      </body>
+    </html>
     `;
   } else {
     message = `
@@ -113,12 +144,15 @@ const fnSendEmail = async (hashToValidateAcount, typePlayer, name, email) => {
     try {
       await mailgun.messages().send(data, function(error, body) {
         if (error) {
+          console.log('error=>>>>>>>>>>>>>>>>: ', error);
           resolve(false);
         } else {
+          console.log('============>>>>>>> body: ', body);
           resolve(true);
         }
       });
     } catch (error) {
+      console.log('============>>>>>>> catch (error): ', error);
       resolve(false);
     }
   });
